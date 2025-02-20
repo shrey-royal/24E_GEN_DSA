78c9a28c2583563db5e8525ccd4f0419e703a5d5
Shrey K <srkkadia@gmail.com>
2025-02-20 16:06:36 +0530

    Binary Search Iterative

--- Linear DS/Searching.c
+++ Linear DS/Searching.c
@@ -14,6 +14,37 @@ int linearSearch(int *arr, int size, int target) {
     return -1;
 }
 
+int binarySearch(int *arr, int low, int high, int target) {
+    // O(log n) -> time complexity
+    // O(1) -> space complexity (if recursive without optimization O(log n))
+
+    // Recursive Approach
+    // TODO:
+
+    // Iterative Approach
+    int count = 0;
+    int mid = (low + high)/2;
+
+    while(arr[mid] != target) {
+        count++;
+        if (low >= high || high < 0) {
+            printf("\nCount = %d\n", count);
+            return -1;
+        }
+
+        //left sub-array
+        if (arr[mid] > target) high = mid-1;
+
+        //right sub-array
+        if (arr[mid] < target) low = mid+1;
+
+        mid = (low + high) / 2;
+    }
+    printf("\nCount = %d\n", count);
+    return mid;
+}
+
+
 void printArray(int *arr, int size) {
     printf("\nArray elements: { ");
     for (int i = 0; i < size; i++) {
@@ -48,9 +79,36 @@ void testLinearSearch() {
     }
 }
 
+void testBinarySearch() {
+    int size;
+
+    printf("\nEnter size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size], target;
+    
+    for (size_t i = 0; i < size; i++) {
+        arr[i] = i+1;
+    }
+    
+    printArray(arr, size);
+
+    printf("\nEnter target: ");
+    scanf("%d", &target);
+
+    int result = binarySearch(arr, 0, size-1, target);
+
+    if(result != -1) {
+        printf("\n%d found at %d index", target, result);
+    } else {
+        printf("\n%d not found", target);
+    }
+}
+
 int main() {
     srand(time(0));
-    testLinearSearch();
+    // testLinearSearch();
+    testBinarySearch();
 
     return 0;
 }
