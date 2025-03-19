9ec2409e25dcbd95ad5fb5b545d63855956c556f
Shrey K <srkkadia@gmail.com>
2025-03-19 16:06:36 +0530

    insertAtPosition, SearchByValue, getLength, reverseList

--- Linear DS/LinkedList/SinglyLL.c
+++ Linear DS/LinkedList/SinglyLL.c
@@ -39,9 +39,62 @@ void insertAtEnd(struct Node** head, int data) {
 
 void insertAtPosition(struct Node** head, int data, int position) {
     if (position < 1) {
-        //
+        printf("Invalid Position.\n");
+        return;
     }
+
+    struct Node* newNode = createNode(data);
+    if (position == 1) {
+        newNode->next = *head;
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    for (int i = 1; i < position - 1; i++) {
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Position is out of bounds.\n");
+        return;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
 }
+
+int searchByValue(struct Node* head, int key) {
+    while (head != NULL) {
+        if (head->data == key) {
+            return 1;
+        }
+        head = head->next;
+    }
+    return 0;
+}
+
+int getLength(struct Node* head) {
+    int count = 0;
+    while (head != NULL) {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+void reverseList(struct Node** head) {
+    struct Node* current = *head, *prev = NULL, *next = NULL;
+    while(current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    *head = prev;
+}
+
+//
 
 void printList(struct Node* head) {
     printf("\nList: ==> ");
@@ -54,7 +107,7 @@ void printList(struct Node* head) {
 
 // Input: 2 22 2 23 2 24 2 25 2 26 2 27 1 21
 int main() {
-    int choice, data;
+    int choice, data, position, key;
     struct Node* head = NULL;
     
     while(choice != 0) {
@@ -87,6 +140,30 @@ int main() {
                 printf("\nEnter the data: ");
                 scanf("%d", &data);
                 insertAtEnd(&head, data);
+                break;
+
+            case 3:
+                printf("\nEnter the position: ");
+                scanf("%d", &position);
+                printf("\nEnter the data: ");
+                scanf("%d", &data);
+                insertAtPosition(&head, data, position);
+                break;
+
+            case 4:
+                printf("\nEnter the key to search from the list: ");
+                scanf("%d", &key);
+                searchByValue(head, key) ? printf("\n%d present in the list...\n", key) : printf("\n%d is not present in the list...\n", key);
+                system("pause");
+                break;
+
+            case 5:
+                printf("\nLength of the list: %d\n", getLength(head));
+                system("pause");
+                break;
+    
+            case 6:
+                reverseList(&head);
                 break;
 
             case 10:
